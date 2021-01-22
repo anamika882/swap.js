e1975b471f6021ad7d9c5e5f500b5bd5cbdd0ab8
Anamika Guha <anmaikaguha33@gmail.com>
2021-01-22 23:03:59 +0600

    js

---
+++ swap.js
@@ -0,0 +1,53 @@
+//kilometerTometer
+function kilometerTometer(kilometer);
+var meters = kilometers*1000;
+return meters;
+
+var result = kilometerTometer()
+
+    console.log(result);
+
+
+//budgetCalculator
+
+function budgetCalculator(expenses);
+var intWatch = 50;
+var intMobile = 100;
+var intLaptop = 100;
+ var intIncome= 1000;
+
+var intCost= intWatch+intMobile+intExpenses;
+
+if (intCost<intIncome){
+     message = "ok";
+}
+
+else{
+    message ="cant bye";
+}
+return message;
+
+console.log(intWatch + "enter the number of watch");
+console.log(intMobile + "enter the number of mobile");
+console.log(intLaptop + "enter the number of laptop");
+
+//megaFriend
+
+
+function megaFriend(arr) {
+    var largestString = ["anamika","oishi","arnob","badhan"];
+    for(var arrayIndex = 0; arrayIndex < arr.length; arrayIndex++) {
+     for(var subArrayIndex = 0; subArrayIndex < arr[arrayIndex].length; subArrayIndex++) {
+        if(arr[arrayIndex][subArrayIndex] > largestString[arrayIndex]) {         
+           largestString[arrayIndex] = arr[arrayIndex][subArrayIndex];
+         }
+     }
+  }
+  return largestNumber;
+ }
+
+
+
+
+    
+    
